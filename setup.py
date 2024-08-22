6e080b29123e14151924a0de3a0d556144aa6e66
mingminghou <mingming1.hou@gmail.com>
2024-08-22 18:09:36 +0800

    Released v0.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 
 setup(
     name='OpsAutomate',  # package name
